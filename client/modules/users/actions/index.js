59248612c62103c330276ed3c505a6367d77c15c
martinwh99 <martinwh99@gmail.com>
2016-03-02 09:50:23 +0100

    user init

---
+++ client/modules/users/actions/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by martin on 02.03.2016.
+ */
+import users from './users';
+
+const actions = {
+    users
+};
+
+export default actions;
---
+++ client/modules/users/actions/users.js
@@ -0,0 +1,20 @@
+/**
+ * Created by martin on 02.03.2016.
+ */
+
+export default {
+    create({Meteor, LocalState}, email, password) {
+        if (!email) {
+            return LocalState.set('CREATE_USER_ERROR', 'Email is required.');
+        }
+
+        if (!password) {
+            return LocalState.set('CREATE_USER_ERROR','Password is required.')
+        }
+
+        LocalState.set('CREATE_USER_ERROR',null);
+
+        Accounts.createUser({email, password});
+        FlowRouter.go('/');
+    }
+}
